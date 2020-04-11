3c4ebdd1dbb91d85a42821248f8bb21e3f184838
Linux Debian Muhammad Raza <k163890@nu.edu.pk>
2020-04-11 15:29:03 +0000

    action model integrated

--- model/Model.py
+++ model/Model.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 
-database_file = "sqlite:///{}".format(os.path.join(project_dir+'\\..', "database.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir+'//..', "database.db"))
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
